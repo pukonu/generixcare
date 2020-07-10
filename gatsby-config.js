2b536468dae96b55e4f9556a7e6f056dc4ae9307
its-tayo <akindolietayo@gmail.com>
2020-07-10 18:25:25 +0100

    feat: add gatsby-source-prismic and page schema

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,23 @@
+const page = require('./src/schemas/page.json');
+
 require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`
 });
 
 module.exports = {
   plugins: [
+    {
+      resolve: 'gatsby-source-prismic',
+      options: {
+        repositoryName: `${process.env.PRISMIC_REPOSITORY}`,
+        accessToken: `${process.env.PRISMIC_API_KEY}`,
+        schemas: {
+          page
+        },
+        // eslint-disable-next-line no-unused-vars
+        linkResolver: ({ node, key, value }) => (doc) => `/${doc.uid}`
+      }
+    },
     'gatsby-plugin-root-import',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components'
